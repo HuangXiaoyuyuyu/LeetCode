3bdbf5e5830c7273b8e6edacf3863452bb648113
Huangxiaoyuyuyu <253757635@qq.com>
2019-07-16 22:01:06 +0800

    LeetCode_0383_赎金信

---
+++ src/LeetCode_0383_赎金信/Solution.java
@@ -0,0 +1,42 @@
+package LeetCode_0383_赎金信;
+
+import java.util.HashMap;
+
+/**
+ * @Author OliverYu
+ * @Date 2019/7/16 21:09
+ * @Email 253757635@qq.com
+ * @Description TODO
+ */
+public class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        int magazineLen = magazine.length();
+        for (int i=0; i<magazineLen; i++) {
+            if (map.containsKey(magazine.charAt(i))) {
+                map.put(magazine.charAt(i),map.get(magazine.charAt(i)) + 1);
+            }else {
+                map.put(magazine.charAt(i),1);
+            }
+        }
+
+        int ransomNoteLen = ransomNote.length();
+        for (int i=0; i<ransomNoteLen; i++) {
+            if (map.containsKey(ransomNote.charAt(i))) {
+                map.put(ransomNote.charAt(i),map.get(ransomNote.charAt(i)) - 1);
+                if (map.get(ransomNote.charAt(i)) < 0) {
+                    return false;
+                }
+            }else {
+               return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String ransomNote = "aa";
+        String magazine = "aab";
+        System.out.println(new Solution().canConstruct(ransomNote,magazine));
+    }
+}
