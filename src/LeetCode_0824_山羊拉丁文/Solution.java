a03828931d06ce1bebcf2fde5fdde03f8217b782
Huangxiaoyuyuyu <253757635@qq.com>
2019-08-03 23:51:45 +0800

    LeetCode_0824_山羊拉丁文

---
+++ src/LeetCode_0824_山羊拉丁文/Solution.java
@@ -0,0 +1,73 @@
+package LeetCode_0824_山羊拉丁文;
+
+/**
+ * @Author OliverYu
+ * @Date 2019/8/3 23:15
+ * @Email 253757635@qq.com
+ * @Description TODO
+ *
+ * 给定一个由空格分割单词的句子 S。每个单词只包含大写或小写字母。
+ * 我们要将句子转换为 “Goat Latin”（一种类似于 猪拉丁文 - Pig Latin 的虚构语言）。
+ *
+ * 山羊拉丁文的规则如下：
+ * 如果单词以元音开头（a, e, i, o, u），在单词后添加"ma"。
+ * 例如，单词"apple"变为"applema"。
+ *
+ * 如果单词以辅音字母开头（即非元音字母），移除第一个字符并将它放到末尾，之后再添加"ma"。
+ * 例如，单词"goat"变为"oatgma"。
+ *
+ * 根据单词在句子中的索引，在单词最后添加与索引相同数量的字母'a'，索引从1开始。
+ * 例如，在第一个单词后添加"a"，在第二个单词后添加"aa"，以此类推。
+ * 返回将 S 转换为山羊拉丁文后的句子。
+ *
+ * 示例 1:
+ *  输入: "I speak Goat Latin"
+ *  输出: "Imaa peaksmaaa oatGmaaaa atinLmaaaaa"
+ *
+ * 示例 2:
+ *  输入: "The quick brown fox jumped over the lazy dog"
+ *  输出: "heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa
+ *       overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa"
+ *
+ * 说明:
+ *  S 中仅包含大小写字母和空格。单词间有且仅有一个空格。
+ *  1 <= S.length <= 150。
+ */
+
+public class Solution {
+    public String toGoatLatin(String S) {
+        StringBuilder stringBuilder = new StringBuilder();
+        StringBuilder aChange = new StringBuilder();
+        StringBuilder returnBuilder = new StringBuilder();
+        String[] strings = S.split(" ");
+        for (int i = 0; i < strings.length; i++) {
+            aChange = aChange.append("a");
+            if (isStartWithVowel(strings[i])) {
+                stringBuilder = stringBuilder.append(strings[i]).append("ma").append(aChange).append(" ");
+            } else {
+                stringBuilder = strings[i].length() == 1 ? stringBuilder.append(strings[i]).append("ma").append(aChange).append(" ") : stringBuilder.append(strings[i].substring(1)).append(strings[i].substring(0, 1)).append("ma").append(aChange).append(" ");
+            }
+            returnBuilder = returnBuilder.append(stringBuilder);
+            stringBuilder.delete(0,stringBuilder.length());
+        }
+        return returnBuilder.toString().trim();
+    }
+
+
+    private boolean isStartWithVowel(String s) {
+        if (s.charAt(0) == 'a' || s.charAt(0) == 'A' ||
+                s.charAt(0) == 'e' || s.charAt(0) == 'E' ||
+                s.charAt(0) == 'i' || s.charAt(0) == 'I' ||
+                s.charAt(0) == 'o' || s.charAt(0) == 'O' ||
+                s.charAt(0) == 'u' || s.charAt(0) == 'U') {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String s = "UXc J K";
+        System.out.println(new Solution().toGoatLatin(s));
+
+    }
+}
