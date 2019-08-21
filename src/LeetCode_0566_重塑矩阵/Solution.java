44bec4605c069b86f86dff1c5e4c67fb1100f56f
Huangxiaoyuyuyu <253757635@qq.com>
2019-08-21 22:20:10 +0800

    LeetCode_0566_重塑矩阵_添加方式二

--- src/LeetCode_0566_重塑矩阵/Solution.java
+++ src/LeetCode_0566_重塑矩阵/Solution.java
@@ -62,4 +62,24 @@ public class Solution {
         }
         return returnArr;
     }
+
+    //方式二：寻找相对位置
+    /*public int[][] matrixReshape(int[][] nums, int r, int c) {
+        int height = nums.length;
+        int width = nums[0].length;
+        int total = height * width;
+        if (total < r * c) {
+            return nums;
+        }
+
+        int[][] res = new int [r][c];
+        int count = 0;
+        for(int i = 0; i < height; i++){
+            for(int j = 0; j < width; j++){
+                res[count / c][count % c] = nums[i][j];
+                count++;
+            }
+        }
+        return res;
+    }*/
 }
