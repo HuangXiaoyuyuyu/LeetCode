e5feccb137402e9406959635b80b0b215b88fca8
Huang xiaoyu <253757635@qq.com>
2020-02-28 22:34:18 +0800

    [增加一种解法]

--- src/LeetCode_0383_赎金信/Solution.java
+++ src/LeetCode_0383_赎金信/Solution.java
@@ -9,26 +9,47 @@ import java.util.HashMap;
  * @Description TODO
  */
 public class Solution {
-    public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character,Integer> map = new HashMap<>();
-        int magazineLen = magazine.length();
-        for (int i=0; i<magazineLen; i++) {
-            if (map.containsKey(magazine.charAt(i))) {
-                map.put(magazine.charAt(i),map.get(magazine.charAt(i)) + 1);
-            }else {
-                map.put(magazine.charAt(i),1);
-            }
-        }
+//    public boolean canConstruct(String ransomNote, String magazine) {
+//        if (ransomNote.length() > magazine.length()) {
+//            return false;
+//        }
+//        HashMap<Character,Integer> map = new HashMap<>();
+//        int magazineLen = magazine.length();
+//        for (int i=0; i<magazineLen; i++) {
+//            if (map.containsKey(magazine.charAt(i))) {
+//                map.put(magazine.charAt(i),map.get(magazine.charAt(i)) + 1);
+//            }else {
+//                map.put(magazine.charAt(i),1);
+//            }
+//        }
+//
+//        int ransomNoteLen = ransomNote.length();
+//        for (int i=0; i<ransomNoteLen; i++) {
+//            if (map.containsKey(ransomNote.charAt(i))) {
+//                map.put(ransomNote.charAt(i),map.get(ransomNote.charAt(i)) - 1);
+//                if (map.get(ransomNote.charAt(i)) < 0) {
+//                    return false;
+//                }
+//            }else {
+//               return false;
+//            }
+//        }
+//        return true;
+//    }
 
-        int ransomNoteLen = ransomNote.length();
-        for (int i=0; i<ransomNoteLen; i++) {
-            if (map.containsKey(ransomNote.charAt(i))) {
-                map.put(ransomNote.charAt(i),map.get(ransomNote.charAt(i)) - 1);
-                if (map.get(ransomNote.charAt(i)) < 0) {
-                    return false;
-                }
-            }else {
-               return false;
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length()) {
+            return false;
+        }
+        int[] tempArr = new int[26];
+        for (int i = 0; i < magazine.length(); i++) {
+            tempArr[magazine.charAt(i) - 97]++;
+        }
+        for (int i = 0; i < ransomNote.length(); i++) {
+            if (tempArr[ransomNote.charAt(i) - 97] > 0) {
+                tempArr[ransomNote.charAt(i) - 97] --;
+            } else {
+                return false;
             }
         }
         return true;
